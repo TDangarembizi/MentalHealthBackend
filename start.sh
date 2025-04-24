728b6f0feb75025377d952ec337465bfc0885fc4
Tino <T0410828@my.ntu.ac.uk>
2025-04-24 08:29:41 +0100

    fixes

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 
 echo "🔁 Starting Mental Health Chatbot Backend..."
 
-cd rasa
-rasa run --enable-api --cors "*" --model models --endpoints endpoints.yml --port 5005 --debug &
+#cd rasa
+#rasa run --enable-api --cors "*" --model models --endpoints endpoints.yml --port 5005 --debug &
 sleep 5
 
-rasa run actions --port 5055 --debug &
-cd ..
+#rasa run actions --port 5055 --debug &
+#cd ..
 
 echo "🚀 Launching Flask..."
 PORT=${PORT:-5000}  # Use Railway's $PORT if defined
